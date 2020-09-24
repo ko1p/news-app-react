d813e949b8c522caf5435913a8c4b7a95d27235d
Anton Belko <30528165+ko1p@users.noreply.github.com>
2020-09-24 12:32:56 +0300

    Добавил попап успешной регистрации. Переработал блок переключения попапов.

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -2,10 +2,20 @@ import React, {useContext} from "react";
 import {Context} from "../../index";
 import PopupSignin from "./PopupSignin";
 import PopupSignup from "./PopupSignup";
+import PopupSuccess from "./PopupSuccess";
 
 
 export default function Popup() {
     const {state} = useContext(Context)
+
+    const renderPopupType = (type) => {
+        switch (type) {
+            case 'signin': return <PopupSignin />
+            case 'signup': return <PopupSignup />
+            case 'success': return <PopupSuccess />
+            default: return state
+        }
+    }
 
     return (
         <>
@@ -13,10 +23,7 @@ export default function Popup() {
                 state.popup.isOpen ?
                     <div className='popup popup_is-opened'>
                         {
-                            state.popup.type === 'signin' ?
-                                <PopupSignin />
-                                :
-                                <PopupSignup />
+                            renderPopupType(state.popup.type)
                         }
                     </div>
                 :
---
+++ src/components/Popup/PopupSuccess.js
@@ -0,0 +1,27 @@
+import React, {useContext} from "react";
+import closeImg from "../../images/close.svg";
+import {Context} from "../../index";
+
+export default function PopupSuccess() {
+    const {dispatch} = useContext(Context)
+
+    const closePopup = () => {
+        dispatch({type: 'CLOSE_POPUP'})
+    }
+
+    const openSigninPopup = () => {
+        dispatch({type: 'OPEN_SIGNIN_POPUP'})
+    }
+
+    return (
+        <div className="popup__container">
+            <img className="popup__close"
+                 onClick={closePopup}
+                 src={closeImg} alt="кнопка закрытия формы"/>
+            <h3 className="popup__heading">Пользователь успешно зарегистрирован</h3>
+            <span
+                onClick={openSigninPopup}
+                className="form__link form__link-success">Выполнить вход</span>
+        </div>
+    )
+}
