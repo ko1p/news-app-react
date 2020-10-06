59c8145ce094d9f3ff025bd278b13a7499a2407c
Anton Belko <30528165+ko1p@users.noreply.github.com>
2020-10-06 11:55:52 +0300

    upload

---
+++ precache-manifest.cd9902edcb73516e83a166d7a11471a4.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf237f3a62c0b01550b398367369404",
+    "url": "./index.html"
+  },
+  {
+    "revision": "604dc80f0f796022c896",
+    "url": "./static/css/main.49475b1c.chunk.css"
+  },
+  {
+    "revision": "8cc719b496ae1b237fdd",
+    "url": "./static/js/2.cb6c2622.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.cb6c2622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604dc80f0f796022c896",
+    "url": "./static/js/main.64cd5299.chunk.js"
+  },
+  {
+    "revision": "608c65a84229406c57b7",
+    "url": "./static/js/runtime-main.57059469.js"
+  },
+  {
+    "revision": "954ff5c553b72c89dfd37a2646f1b97c",
+    "url": "./static/media/bookmark-hover.954ff5c5.svg"
+  },
+  {
+    "revision": "fb293bd019c7947efdb75a6245ae8765",
+    "url": "./static/media/bookmark-marked.fb293bd0.svg"
+  },
+  {
+    "revision": "4adfd40981683e3b6be449d062ac949b",
+    "url": "./static/media/bookmark.4adfd409.svg"
+  },
+  {
+    "revision": "d94383b574906bf6c3cca5cc20f63d55",
+    "url": "./static/media/close.d94383b5.svg"
+  },
+  {
+    "revision": "020061501b284cac060ef1076ab3fa87",
+    "url": "./static/media/delete-hover.02006150.svg"
+  },
+  {
+    "revision": "d01f9f0b44429abb4f2b8df08affee9a",
+    "url": "./static/media/delete.d01f9f0b.svg"
+  },
+  {
+    "revision": "d01f9f0b44429abb4f2b8df08affee9a",
+    "url": "./static/media/deleteIcon_not-hovered.d01f9f0b.svg"
+  },
+  {
+    "revision": "48a326455c8e879ecc652b0af85112d2",
+    "url": "./static/media/fb.48a32645.svg"
+  },
+  {
+    "revision": "cab2ca754cc549c6d620006d73ce71ac",
+    "url": "./static/media/georgia-de-lotz--UsJoNxLaNo-unsplash.cab2ca75.jpg"
+  },
+  {
+    "revision": "e189624aa400563199e32e37716e0806",
+    "url": "./static/media/github.e189624a.svg"
+  },
+  {
+    "revision": "2058fecc3297e25de2e258224fe1bd4a",
+    "url": "./static/media/image-03-hq.2058fecc.jpg"
+  },
+  {
+    "revision": "11434dc4b74782800a665b427b6b8b2f",
+    "url": "./static/media/image-03.11434dc4.jpg"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "./static/media/inter-regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "175a96ce472d1ca73e7bfaaabddbb721",
+    "url": "./static/media/not-found.175a96ce.svg"
+  },
+  {
+    "revision": "39d7226d1f440dd7e02dd0c0424e6b43",
+    "url": "./static/media/roboto-black.39d7226d.woff"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "./static/media/roboto-medium.6422eac7.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "./static/media/robotoslab-regular.594bcf8b.woff"
+  },
+  {
+    "revision": "80cd9730b0e1eec5487390c0c7b90210",
+    "url": "./static/media/saveIcon_hovered.80cd9730.svg"
+  },
+  {
+    "revision": "f975e5869f455e928c17ce8e2ea17e0c",
+    "url": "./static/media/saveIcon_marked.f975e586.svg"
+  },
+  {
+    "revision": "4bc36d6acfe002532b681ed4f98e6404",
+    "url": "./static/media/saveIcon_not-hovered.4bc36d6a.svg"
+  },
+  {
+    "revision": "49d8d8285e8bc3047dac602de489bfb0",
+    "url": "./static/media/sourcesanspro-regular.49d8d828.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b8ed2fc4fda10bb49e65899572c72dfd.js"
+  "./precache-manifest.cd9902edcb73516e83a166d7a11471a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
